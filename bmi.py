57d786dd4041bfc9160668970fca0de2700c439c
IsaiahSantiago <IsaiahSantiago456@gmail.com>
2023-10-25 17:54:25 -0400

    BMI done

---
+++ bmi.py
@@ -0,0 +1,13 @@
+import sys 
+W = input("Enter your weight (in pounds) ")
+H = input("Okay, also enter your height (in inches) ")
+BMI = (703 * float(W)) / (float(H) * float(H))   
+print("Your body mass index (BMI) is " + str(BMI) ) 
+
+
+# we gotta use FLOAT's
+
+
+
+
+
