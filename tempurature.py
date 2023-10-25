d2f897249bee2b78bd040726823f313d7579911c
IsaiahSantiago <IsaiahSantiago456@gmail.com>
2023-10-25 13:36:04 -0400

    Tempurature PY established

---
+++ tempurature.py
@@ -0,0 +1,8 @@
+tempincelsius = input("Please enter a temperature in celsius: ")  
+tempinfahrenheit = (float(tempincelsius) * (9 / 5)) + 32.0   
+print("The temperature is " + str(tempinfahrenheit) + "°F") 
+# print(type(tempincelsius))
+
+  # tempinfahrenheit = (tempincelsius * (9 / 5)) + 32.0  
+  # print("The temperature is " + tempinfahrenheit + "°F") 
+
